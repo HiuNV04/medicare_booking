7882830a519c0b164536489aa8a9f15275e0c849
HiuNV04 <huynvhe182260@fpt.edu.vn>
2025-06-24 15:01:30 +0700

    commit

---
+++ src/java/model/Room.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Room {
+    private int id;
+    private int doctorId;  
+    private String name;
+
+    public Room() {
+    }
+
+    public Room(int id, int doctorId, String name) {
+        this.id = id;
+        this.doctorId = doctorId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
