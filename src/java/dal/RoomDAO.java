e770fb0711f0a2220441712e41281c28174822af
HiuNV04 <huynvhe182260@fpt.edu.vn>
2025-06-26 20:35:42 +0700

    cm

--- src/java/dal/RoomDAO.java
+++ src/java/dal/RoomDAO.java
@@ -8,10 +8,7 @@ package dal;
  *
  * @author ADMIN
  */
-<<<<<<<< HEAD:src/java/model/a.java
-public class a {
-========
+
 public class RoomDAO extends MyDAO {
->>>>>>>> master:src/java/dal/RoomDAO.java
     
 }
